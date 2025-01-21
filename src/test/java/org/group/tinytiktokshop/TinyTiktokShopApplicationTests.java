996699bf12dc32efb720fd75da95d12945752446
Capsfly <2018640800@qq.com>
2025-01-21 17:42:42 +0800

    Init repo (#1)
    * initRepo
    
    ---------
    
    Co-authored-by: Chunyu <chunyusui@outlook.com>

---
+++ src/test/java/org/group/tinytiktokshop/TinyTiktokShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group.tinytiktokshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTiktokShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
